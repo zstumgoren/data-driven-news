69fe3997bf4f8e199a4c80cb441294ccbe2337be
Serdar Tumgoren <zstumgoren@gmail.com>
2019-04-18 11:27:05 -0700

    Update markdown template engine config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,36 @@
+const pluginTOC = require('eleventy-plugin-nesting-toc');
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(
+    pluginTOC,
+    {tags: ['h2', 'h3'], wrapper: "aside"}
+  );
 
   eleventyConfig.addPassthroughCopy("css");
+
+	/* Markdown */
+	let markdownIt = require("markdown-it");
+	let markdownItAnchor = require("markdown-it-anchor");
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true
+	};
+	let opts = {
+    permalink: false,
+		slugify: function(s) {
+			let newStr = String(s).replace(/New\ in\ v\d+\.\d+\.\d+/, '');
+			newStr = newStr.replace(/⚠️/g, '');
+			newStr = newStr.replace(/[?!]/g, '');
+			return encodeURIComponent(newStr.trim().toLowerCase().replace(/\s+/g, '-'));
+		},
+		permalinkClass: "direct-link",
+		level: [1,2,3]
+	};
+
+	eleventyConfig.setLibrary("md", markdownIt(options)
+		.use(markdownItAnchor, opts)
+	);
 
   return {
     templateFormats: [
@@ -10,7 +40,7 @@ module.exports = function(eleventyConfig) {
       "liquid"
     ],
     pathPrefix: "/",
-    markdownTemplateEngine: "liquid",
+    markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
     passthroughFileCopy: true,
