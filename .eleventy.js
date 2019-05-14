3e5716c35436e4eb22df767e9f945fcce3bd33c5
Serdar Tumgoren <zstumgoren@gmail.com>
2019-05-14 09:44:53 -0700

    Add support for static pdfs

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("static/pdf");
 
 	/* Markdown */
 	let markdownIt = require("markdown-it");
